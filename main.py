93c55fc973eed0ddfe48bcf9c288728769ef6299
Olga-Pro <oip_@inbox.ru>
2024-06-03 16:27:07 +0300

    add_code

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def cur_date_time():
+    now = datetime.now()
+    current_time = now.strftime("%d.%m.%Y %H:%M:%S")
+
+    return f"<h1>Сейчас: {current_time} </h1>"
+
+if __name__ == '__main__':
+    app.run()
